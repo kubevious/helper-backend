2282f6c52bca2969f907918416ecfd7ae0676637
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-12-10 13:53:09 -0800

    added exception and uncaught rejection handling

--- src/backend.ts
+++ src/backend.ts
@@ -5,6 +5,7 @@ import { ILogger, RootLogger, setupRootLogger, LoggerOptions, LogLevel } from 't
 import { Resolvable, Promise } from 'the-promise';
 import _ from 'the-lodash';
 import { v4 as uuidv4 } from 'uuid';
+import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 export type TimerFunction = () => Resolvable<any>
 
@@ -13,21 +14,25 @@ export class Backend {
     private _logger: ILogger;
     private _timers: Record<string, NodeJS.Timeout> = {};
     private _intervals: NodeJS.Timeout[] = [];
+    private _errorHandler? : (reason: any) => any;
 
     constructor(name: string) {
         // Process Setup
         process.stdin.resume();
-        process.on('exit', this._exitHandler.bind(this, { cleanup: true }));
+        process.on('exit', this._exitHandler.bind(this, { event: 'exit', cleanup: true }));
 
         //catches ctrl+c event
-        process.on('SIGINT', this._exitHandler.bind(this, { exit: true }));
+        process.on('SIGINT', this._exitHandler.bind(this, { event: 'SIGINT', exit: true }));
 
         // catches "kill pid" (for example: nodemon restart)
-        process.on('SIGUSR1', this._exitHandler.bind(this, { exit: true }));
-        process.on('SIGUSR2', this._exitHandler.bind(this, { exit: true }));
+        process.on('SIGUSR1', this._exitHandler.bind(this, { event: 'SIGUSR1', exit: true }));
+        process.on('SIGUSR2', this._exitHandler.bind(this, { event: 'SIGUSR2', exit: true }));
 
         //catches uncaught exceptions
-        process.on('uncaughtException', this._exitHandler.bind(this, { exit: true }));
+        process.on('uncaughtException', this._uncaughtException.bind(this));
+
+        //catches uncaught exceptions
+        process.on('unhandledRejection', this._unhandledRejection.bind(this));
 
         // Logging
         const loggerOptions = new LoggerOptions();
@@ -53,17 +58,22 @@ export class Backend {
 
     initialize(cb : () => any)
     {
-        this.logger.error('[Backend::initialize] Begin.');
+        this.logger.info('[Backend::initialize] Begin.');
         Promise.resolve()
             .then(() => cb())
             .then(() => {
-                this.logger.error('[Backend::initialize] End.');
+                this.logger.info('[Backend::initialize] End.');
             })
             .catch((reason : any) => {
                 this.logger.error('[Backend::initialize] FAILED. Application will now exit. Reason: ', reason);
-                this.close();
+                return this._handleError(reason);
             })
             .then(() => null);
+    }
+
+    registerErrorHandler(errorHandler? : (reason: any) => any)
+    {
+        this._errorHandler = errorHandler;
     }
 
     close() {
@@ -92,6 +102,38 @@ export class Backend {
         }, timeout);
 
         this._intervals.push(timerObj);
+    }
+
+    private _uncaughtException(reason: any)
+    {
+        this.logger.error('[_uncaughtException] Reason: ', reason);
+        this._handleError(reason);
+    }
+
+    private _unhandledRejection(reason: any)
+    {
+        this.logger.error('[_unhandledRejection] Reason: ', reason);
+        this._handleError(reason);
+    }
+
+    private _handleError(reason: any)
+    {
+        this.logger.error('[_handleError] Reason: ', reason);
+
+        Promise.resolve()
+            .then(() => {
+                if (this._errorHandler)
+                {
+                    return this._errorHandler!(reason);
+                }
+            })
+            .catch((reason : any) => {
+                this.logger.error('[Backend::_handleError] FAILED. Reason:', reason);
+            })
+            .then(() => {
+                this.close();
+            })
+            .then(() => null);
     }
 
     private _triggerCallback(cb: TimerFunction)
@@ -127,6 +169,8 @@ export class Backend {
     }
 
     private _exitHandler(options: any, exitCode: any) {
+        console.log('[Backend::_exitHandler] event: %s', options.event);
+
         if (options.cleanup) {
             console.log('[Backend::_exitHandler] cleanup');
         }
