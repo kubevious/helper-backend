cdf57eeffa6a2a5f9b49d79814feeb575c4a732d
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-03-31 21:54:22 -0700

    exported types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 export { Backend, TimerFunction } from './backend';
 
+export { Express, Request, Response, Router as ExpressRouter, NextFunction } from 'express';
+
 export { Server } from './server/server';
 export { Router } from './server/router';
 export { RouterError, ErrorReporter } from './server/router-error';
-export { Express, Request, Response, Router as ExpressRouter, NextFunction } from 'express';
+export { MiddlewareName, MiddlewareRef } from './server';
+export { MiddlewareCallbackFunc, MiddlewarePromiseFunc } from './server';
+export { MiddlewareBuilderArgs, MiddlewareFunctionBuilder, MiddlewarePromiseBuilder } from './server';
+
+
+
